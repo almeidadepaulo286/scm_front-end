d68d369add32b8cfde5c66db52a5d1da76c43b08
Oliveira Developer <ramo.ltda@gmail.com>
2020-10-21 14:52:03 -0300

    Correcao do posicionamento do icon-hamburguer para controle da menu-side-bar.

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -9,39 +9,56 @@ import { faOutdent, faIndent, faBars, faUser } from '@fortawesome/free-solid-svg
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-    public pushRightClass: string;
 
-    usuario: string;
     faOutdent = faOutdent;
     faIndent = faIndent;
     faBars = faBars;
     faUser = faUser;
-    sidebarIsOpen = false;
 
-    message: string = "Hello!"
+    usuario: string;
+    sidebarIsMinimized = false;
 
     constructor(public router: Router) {
     }
 
     ngOnInit() {
+        this.sidebarIsMinimized = jQuery(".page-sidebar .x-navigation").hasClass("x-navigation-minimized");
+
         let user:string = localStorage.getItem('currentUser');
         this.usuario = JSON.parse(user).nome;
-        this.pushRightClass = 'push-right';
     }
 
     toggleIcon() {
-        this.sidebarIsOpen = ! this.sidebarIsOpen;
-
         jQuery("#sidebar-wrapper").toggleClass("page-sidebar-minimized");
-        jQuery(".page-sidebar .x-navigation").hasClass("x-navigation-minimized") ? this.x_navigation_minimize("open") : this.x_navigation_minimize("close");
+
+        if (jQuery(".page-sidebar .x-navigation").hasClass("x-navigation-minimized")) {
+            this.x_navigation_minimize("open");
+         } else {
+            this.x_navigation_minimize("close");
+         }
 
         console.log("toggleIcon no header.component.ts");
     }
 
     x_navigation_minimize(n : string) {
-        n == "open" && (jQuery(".page-container").removeClass("page-navigation-minimized"), jQuery(".page-sidebar").removeClass("page-sidebar-minimized"), jQuery(".page-sidebar .x-navigation").removeClass("x-navigation-minimized"), jQuery(".x-navigation-minimize").find(".fa").removeClass("fa-indent").addClass("fa-dedent")); //, jQuery(".page-sidebar.scroll").mCustomScrollbar("update"), setCookie("sidebar_minimized", "false", 365));
-
-        n == "close" && (jQuery(".page-container").addClass("page-navigation-minimized"), jQuery(".page-sidebar").addClass("page-sidebar-minimized"), jQuery(".page-sidebar .x-navigation").addClass("x-navigation-minimized"), jQuery(".x-navigation-minimize").find(".fa").removeClass("fa-dedent").addClass("fa-indent")); //, jQuery(".page-sidebar.scroll").mCustomScrollbar("disable", !0), setCookie("sidebar_minimized", "true", 365));
+        if (n == "open") {
+            this.sidebarIsMinimized = false;
+            jQuery(".page-container").removeClass("page-navigation-minimized");
+            jQuery(".page-sidebar").removeClass("page-sidebar-minimized");
+            jQuery(".page-sidebar .x-navigation").removeClass("x-navigation-minimized");
+            jQuery(".x-navigation-minimize").find(".fa").removeClass("fa-indent").addClass("fa-dedent");
+            //jQuery(".page-sidebar.scroll").mCustomScrollbar("update");
+            //setCookie("sidebar_minimized", "false", 365);
+        
+        } else if (n == "close") {
+            this.sidebarIsMinimized = true;
+            jQuery(".page-container").addClass("page-navigation-minimized");
+            jQuery(".page-sidebar").addClass("page-sidebar-minimized");
+            jQuery(".page-sidebar .x-navigation").addClass("x-navigation-minimized");
+            jQuery(".x-navigation-minimize").find(".fa").removeClass("fa-dedent").addClass("fa-indent");
+            //jQuery(".page-sidebar.scroll").mCustomScrollbar("disable", !0);
+            //setCookie("sidebar_minimized", "true", 365);
+        }
 
         jQuery(".x-navigation li.active").removeClass("active")
     }
